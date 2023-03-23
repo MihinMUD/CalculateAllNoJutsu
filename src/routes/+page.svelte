7e78e97a8f42e25d4f1ee486d534a84147fff91f
MihinMUD <mihinudissanayake@gmail.com>
2023-03-23 18:42:34 +0530

    Added basic functionality

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,22 @@
 <h1>Calculate All No Jutsu!</h1>
 
-<input type="text" placeholder="Your Math Problem">
-
-<script>
-    
+<form>
+    <input bind:value={prob} type="text" placeholder="Your Math Problem">
+    <input type="submit" on:click={calc} value="Calculate!">
+    <p>{ans}</p>
+</form>
+<script lang='ts'>
+    import {evaluate} from 'mathjs'
+    let prob:any;
+    let ans:String ="";
+    let calc = () => {
+        try {
+            ans = evaluate(prob)
+        } catch {
+            ans = "Invalid syntax!"
+        }
+        prob = ""
+    }
 </script>
 
 <style></style>
